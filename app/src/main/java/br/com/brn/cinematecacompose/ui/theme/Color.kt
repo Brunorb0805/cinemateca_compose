44ab16efc2b28300f9c7babf91bf282545cd949a
brunorb <brunorb0805@gmail.com>
2023-10-03 20:44:28 -0300

    Setup Theme

--- app/src/main/java/br/com/brn/cinematecacompose/ui/theme/Color.kt
+++ app/src/main/java/br/com/brn/cinematecacompose/ui/theme/Color.kt
@@ -1,11 +1,100 @@
 package br.com.brn.cinematecacompose.ui.theme
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+import androidx.compose.material.Colors
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
+import br.com.brn.cinematecacompose.R
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DarkColorScheme
+    @Composable get() = darkColorScheme(
+        primary = colorResource(id = R.color.dark_primaryColor),
+        primaryContainer = colorResource(id = R.color.dark_primaryLightColor),
+        onPrimary = colorResource(id = R.color.dark_onPrimaryColor),
+        secondary = colorResource(id = R.color.dark_secondaryColor),
+        secondaryContainer = colorResource(id = R.color.dark_secondaryLightColor),
+        onSecondary = colorResource(id = R.color.dark_onSecondaryColor),
+        tertiary = colorResource(id = R.color.dark_tertiaryColor),
+        tertiaryContainer = colorResource(id = R.color.dark_tertiaryLightColor),
+        onTertiary = colorResource(id = R.color.dark_onTertiaryColor),
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+        background = colorResource(id = R.color.dark_backgroundColor),
+        surface = colorResource(id = R.color.dark_surfaceColor),
+        error = colorResource(id = R.color.dark_errorColor),
+
+//        onBackground = colorResource(id = R.color.dark_onBackgroundColor),
+        onSurface = colorResource(id = R.color.dark_onSurfaceColor),
+        onErrorContainer = colorResource(id = R.color.dark_onErrorColor)
+    )
+
+val LightColorScheme
+    @Composable get() = lightColorScheme(
+        primary = colorResource(id = R.color.primaryColor),
+        primaryContainer = colorResource(id = R.color.primaryLightColor),
+        onPrimary = colorResource(id = R.color.onPrimaryColor),
+        secondary = colorResource(id = R.color.secondaryColor),
+        secondaryContainer = colorResource(id = R.color.secondaryLightColor),
+        onSecondary = colorResource(id = R.color.onSecondaryColor),
+        tertiary = colorResource(id = R.color.tertiaryColor),
+        tertiaryContainer = colorResource(id = R.color.tertiaryLightColor),
+        onTertiary = colorResource(id = R.color.onTertiaryColor),
+
+        background = colorResource(id = R.color.backgroundColor),
+        surface = colorResource(id = R.color.surfaceColor),
+        error = colorResource(id = R.color.errorColor),
+
+//        onBackground = colorResource(id = R.color.onBackgroundColor),
+        onSurface = colorResource(id = R.color.onSurfaceColor),
+        onErrorContainer = colorResource(id = R.color.onErrorColor)
+    )
+
+
+/**
+ * Base class to extend the material color theme with auxiliary colors that goes
+ * beyond the default theme acc. to accomplish your company's corporate identity design
+ */
+@Immutable
+data class ExtendedColors(
+    val snowWhite: Color,
+//    val deepOcean: Color,
+//    val skyBlue: Color,
+//    val nightBlue: Color,
+//    val dialogBackground: Color
+)
+
+// cores que serão usados de acordo com a modalidade dia ou noite
+val LocalExtendedColors = staticCompositionLocalOf {
+    ExtendedColors(
+        snowWhite = Color.Unspecified,
+//        deepOcean = Color.Unspecified,
+//        skyBlue = Color.Unspecified,
+//        nightBlue = Color.Unspecified,
+//        dialogBackground = Color.Unspecified
+    )
+}
+
+// day colors extented - cores auxiliares para dia que vão além do padrão
+val lightExtendedColors: ExtendedColors
+    @Composable
+    get() = ExtendedColors(
+        snowWhite = colorResource(id = R.color.white_snow),
+//        deepOcean = colorResource(id = R.color.blue_deep_ocean),
+//        skyBlue = colorResource(id = R.color.blue_sky),
+//        nightBlue = colorResource(id = R.color.blue_night),
+//        dialogBackground = colorResource(id = R.color.dialog_background)
+    )
+
+// night colors extented - cores auxiliares para dia que vão além do padrão
+val darkExtendedColors: ExtendedColors
+    @Composable
+    get() = ExtendedColors(
+        snowWhite = colorResource(id = R.color.white_snow),
+//        deepOcean = colorResource(id = R.color.black_inverted_mode),
+//        skyBlue = colorResource(id = R.color.blue_sky),
+//        nightBlue = colorResource(id = R.color.black_plain),
+//        dialogBackground = colorResource(id = R.color.black_inverted_mode)
+    )
--- app/src/main/java/br/com/brn/cinematecacompose/ui/theme/Theme.kt
+++ app/src/main/java/br/com/brn/cinematecacompose/ui/theme/Theme.kt
@@ -1,54 +1,47 @@
 package br.com.brn.cinematecacompose.ui.theme
 
 import android.app.Activity
-import android.os.Build
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun CinematecaComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+//    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+
+    val systemUiController = rememberSystemUiController()
+    val extendedColors = if (darkTheme) darkExtendedColors else lightExtendedColors
+
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+        //Pega a paleta de cores de acordo com plano de fundo da tela inicial do SO
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
@@ -56,15 +49,119 @@ fun CinematecaComposeTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
+            //Setando a cor da StatusBar
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+
+            //Seta a cor da Toolbar
+            systemUiController.setSystemBarsColor(
+                color = colorScheme.primary
+            )
+            //Seta a cor do NavigationBar
+            systemUiController.setNavigationBarColor(
+                color = Color.Black
+            )
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(LocalExtendedColors provides extendedColors) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
+
+}
+
+object CinematecaComposeThemeExtended {
+//    val typography: AppCustomTypography
+//        @Composable
+//        get() = AppCustomTypography
+
+    val colors: ExtendedColors
+        @Composable
+        get() = LocalExtendedColors.current
+}
+
+@Preview
+@Composable
+fun LightColorsPreview() {
+    CinematecaComposeTheme(darkTheme = false) {
+        ColorList()
+    }
+}
+
+@Preview
+@Composable
+fun DarkColorsPreview() {
+    CinematecaComposeTheme(darkTheme = true) {
+        ColorList()
+    }
+}
+
+@Composable
+fun ColorList() {
+    Column(
+        modifier = Modifier
+            .verticalScroll(state = rememberScrollState())
+            .fillMaxHeight()
+    ) {
+        mapOf(
+            "primary" to MaterialTheme.colorScheme.primary,
+            "primaryVariant" to MaterialTheme.colorScheme.primaryContainer,
+            "primarySurface" to MaterialTheme.colorScheme.onPrimaryContainer,
+            "onPrimary" to MaterialTheme.colorScheme.onPrimary,
+            "inversePrimary" to MaterialTheme.colorScheme.inversePrimary,
+
+            "secondary" to MaterialTheme.colorScheme.secondary,
+            "onSecondary" to MaterialTheme.colorScheme.onSecondary,
+            "secondaryVariant" to MaterialTheme.colorScheme.secondaryContainer,
+            "onSecondaryContainer" to MaterialTheme.colorScheme.onSecondaryContainer,
+
+            "tertiary" to MaterialTheme.colorScheme.tertiary,
+            "onTertiary" to MaterialTheme.colorScheme.onTertiary,
+            "tertiaryContainer" to MaterialTheme.colorScheme.tertiaryContainer,
+            "onTertiaryContainer" to MaterialTheme.colorScheme.onTertiaryContainer,
+
+            "background" to MaterialTheme.colorScheme.background,
+            "onBackground" to MaterialTheme.colorScheme.onBackground,
+
+            "surface" to MaterialTheme.colorScheme.surface,
+            "onSurface" to MaterialTheme.colorScheme.onSurface,
+            "surfaceVariant" to MaterialTheme.colorScheme.surfaceVariant,
+            "onSurfaceVariant" to MaterialTheme.colorScheme.onSurfaceVariant,
+            "surfaceTint" to MaterialTheme.colorScheme.surfaceTint,
+            "inverseSurface" to MaterialTheme.colorScheme.inverseSurface,
+            "inverseOnSurface" to MaterialTheme.colorScheme.inverseOnSurface,
+
+            "error" to MaterialTheme.colorScheme.error,
+            "onError" to MaterialTheme.colorScheme.onError,
+            "errorContainer" to MaterialTheme.colorScheme.errorContainer,
+            "onErrorContainer" to MaterialTheme.colorScheme.onErrorContainer,
+            "outline" to MaterialTheme.colorScheme.outline,
+
+            // custom extented colors
+            "customSnowWhite" to CinematecaComposeThemeExtended.colors.snowWhite,
+//            "customDeepOcean" to ArsenalThemeExtended.colors.deepOcean,
+//            "customSkyBlue" to ArsenalThemeExtended.colors.skyBlue,
+//            "customNightBlue" to ArsenalThemeExtended.colors.nightBlue,
+//            "customDialogBackground" to ArsenalThemeExtended.colors.dialogBackground,
+        ).forEach { (text, color) ->
+            Row {
+                Text(
+                    text = text,
+                    color = CinematecaComposeThemeExtended.colors.snowWhite,
+                    modifier = Modifier.weight(1f)
+                )
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(32.dp)
+                        .background(color)
+                )
+            }
+        }
+    }
 }
